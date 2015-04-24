05255525d76dd8662b6535a1ec4f7ebed8b1cc9a
imufun <imufun1@gmail.com>
2015-04-24 09:58:53 +0600

    spinner set on addapter charSequence

--- app/src/main/java/com/imufun/imran/converterapp/MainActivity.java
+++ app/src/main/java/com/imufun/imran/converterapp/MainActivity.java
@@ -4,6 +4,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -39,6 +40,16 @@ public class MainActivity extends ActionBarActivity {
 
     private void addItemsToUnitTypeSpinner() {
 
+        unittypeSpinner=(Spinner)findViewById(R.id.Unit_type_spinner);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence>  unitypeAdapter=ArrayAdapter.createFromResource(this,R.array.conversion_types,android.R.layout.simple_spinner_item);
+
+        // Specify the layout to use when the list of choices appears
+        unitypeAdapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        // Apply the adapter to the spinner
+        unittypeSpinner.setAdapter(unitypeAdapter);
     }
 
     private void addListenerToUnitTypeSpinner(){
